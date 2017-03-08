0846d28a0a9d5312fe338fe747887d705bf417e7
JoeMalone <forsikringsmanden@gmail.com>
2017-03-08 12:21:56 +0100

    Final Version

---
+++ pokedex3/Constants.swift
@@ -0,0 +1,17 @@
+//
+//  Constants.swift
+//  pokedex3
+//
+//  Created by Jonas Elholm on 05/03/2017.
+//  Copyright © 2017 ButlerStar. All rights reserved.
+//
+
+import Foundation
+
+let URL_BASE = "http://pokeapi.co"
+
+let URL_POKEMON = "/api/v1/pokemon/"
+
+typealias DownloadComplete = () -> ()
+
+
